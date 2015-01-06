88fe3efda7f7dafa966e36c9551940556819c5a4
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-01-06 17:30:12 +0200

    no duplicate game mechanics

--- rgm.js
+++ rgm.js
@@ -4,7 +4,7 @@ var themes = ["Cyberpunk", "Steampunk", "Fantasy", "Biology"];
 function loadGameTemplate() {
   $.get('game.mst', function(template) {
     var rendered = Mustache.render(template, {
-     mechs: nRandomItems(gameMechanics,3),
+     mechs: nRandomMechanics(gameMechanics,3),
      theme: randomTheme()
      });
     $('#target').html(rendered);
@@ -23,17 +23,24 @@ function randomItem(array) {
     return array[Math.floor(Math.random()*array.length)];
 }
 
-function nRandomItems(array,n){
+function nRandomMechanics(array,n){
 	var items = [];
 
 	while(items.length < n) {
 		var item = randomItem(array);
-		if($.inArray(item, array) != -1){
+		console.log(item);
+		if($.inArray(item, items) == -1){
 			items.push(item);
 		}
 	};
+
+	objectItems = [];
+	for (var i = items.length - 1; i >= 0; i--) {
+		var object = {mech: items[i]};
+		objectItems.push(object);
+	};
 	
-	return items;
+	return objectItems;
 }
 
 loadGameTemplate();
